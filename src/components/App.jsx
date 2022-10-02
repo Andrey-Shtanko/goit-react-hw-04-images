2eebf92d8628032613dbad8c4e1ba34f724caebd
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-10-02 23:36:36 +0300

    add-settings-and-dependensies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Container } from './App.styled';
+
+export class App extends Component {
+  state = {
+    page: 1,
+    images: [],
+  };
+  render() {
+    return <Container>hello world</Container>;
+  }
+}
