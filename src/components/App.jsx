be9fd9d3c2926459442f059f6bba31c3b888e716
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-10-09 20:54:57 +0300

    refactoring-on-hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from './App.styled';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -7,124 +7,144 @@ import { Button } from './Button/Button';
 import { ModalWindow } from './Modal/Modal';
 import { response } from './../services/imageApi';
 
-// export const App = () => {
-//   const [query, setQuery] = useState('');
-//   const [page, setPage] = useState(1);
-//   const [images, setImages] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [largeImageUrl, setLargeImageUrl] = useState(null);
-//   const [largeImageTags, setLargeImageTags] = useState(null);
+export const App = () => {
+  const [query, setQuery] = useState(null);
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [largeImageUrl, setLargeImageUrl] = useState(null);
+  const [largeImageTags, setLargeImageTags] = useState(null);
 
-//   const onSubmit = query => {
-//     setQuery(query);
-//     setPage(1);
-//   };
-//   const onImageClick = (url, tags) => {
-//     setLargeImageUrl(url);
-//     setLargeImageTags(tags);
-//   };
-//   const onHandleClose = () => {
-//     setLargeImageUrl(null);
-//     setLargeImageTags(null);
-//   };
-//   const onChangePage = () => {
-//     setPage(prevState => prevState + 1);
-//   };
-
-//   return (
-//     <Container>
-//       <Searchbar onSubmit={onSubmit} />
-//       {isLoading && <Loader />}
-//       {images.length > 1 && (
-//         <ImageGallery images={images} onImageClick={onImageClick} />
-//       )}
-//       {images.length > 1 && <Button onChangePage={onChangePage} />}
-//       {largeImageUrl && (
-//         <ModalWindow
-//           onHandleClose={onHandleClose}
-//           url={largeImageUrl}
-//           tags={largeImageTags}
-//         />
-//       )}
-//     </Container>
-//   );
-// };
-
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    images: [],
-    isLoading: false,
-    largeImage: {
-      url: null,
-      tags: null,
-    },
-  };
-
-  onFetch = async () => {
-    try {
-      this.setState({ isLoading: true });
-      const { hits } = await response(this.state.query, this.state.page);
-      this.setState(prevState => ({
-        images: [...prevState.images, ...hits],
-      }));
-    } catch (error) {
-      console.log(error);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.query !== this.state.query) {
-      this.setState({ images: [] });
-      this.onFetch();
-
+  useEffect(() => {
+    if (query === null) {
       return;
     }
-    if (
-      prevState.query === this.state.query &&
-      prevState.page !== this.state.page
-    ) {
-      this.onFetch();
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const { hits } = await response(query, page);
+        setImages(prevState => [...prevState, ...hits]);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
-  }
-  onSubmit = query => {
-    this.setState({ query });
-    this.setState({ page: 1 });
+
+    fetchData();
+  }, [page, query]);
+
+  const onSubmit = query => {
+    setQuery(query);
+    setImages([]);
+    setPage(1);
   };
-  onImageClick = (url, tags) => {
-    this.setState({ largeImage: { url, tags } });
+  const onImageClick = (url, tags) => {
+    setLargeImageUrl(url);
+    setLargeImageTags(tags);
   };
-  onHandleClose = () => {
-    this.setState({ largeImage: { url: null, tags: null } });
+  const onHandleClose = () => {
+    setLargeImageUrl(null);
+    setLargeImageTags(null);
   };
-  onChangePage = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const onChangePage = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  render() {
-    return (
-      <Container>
-        <Searchbar onSubmit={this.onSubmit} />
-        {this.state.isLoading && <Loader />}
-        {this.state.images.length > 1 && (
-          <ImageGallery
-            images={this.state.images}
-            onImageClick={this.onImageClick}
-          />
-        )}
-        {this.state.images.length > 1 && (
-          <Button onChangePage={this.onChangePage} />
-        )}
-        {this.state.largeImage.url && (
-          <ModalWindow
-            onHandleClose={this.onHandleClose}
-            url={this.state.largeImage.url}
-            tags={this.state.largeImage.tags}
-          />
-        )}
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Searchbar onSubmit={onSubmit} />
+      {isLoading && <Loader />}
+      {images.length > 1 && (
+        <ImageGallery images={images} onImageClick={onImageClick} />
+      )}
+      {images.length > 1 && <Button onChangePage={onChangePage} />}
+      {largeImageUrl && (
+        <ModalWindow
+          onHandleClose={onHandleClose}
+          url={largeImageUrl}
+          tags={largeImageTags}
+        />
+      )}
+    </Container>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     query: '',
+//     page: 1,
+//     images: [],
+//     isLoading: false,
+//     largeImage: {
+//       url: null,
+//       tags: null,
+//     },
+//   };
+
+// onFetch = async () => {
+//   try {
+//     this.setState({ isLoading: true });
+//     const { hits } = await response(this.state.query, this.state.page);
+//     this.setState(prevState => ({
+//       images: [...prevState.images, ...hits],
+//     }));
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     this.setState({ isLoading: false });
+//   }
+// };
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.query !== this.state.query) {
+// this.setState({ images: [] });
+// this.onFetch();
+
+//       return;
+//     }
+//     if (
+//       prevState.query === this.state.query &&
+//       prevState.page !== this.state.page
+//     ) {
+//       this.onFetch();
+//     }
+//   }
+//   onSubmit = query => {
+//     this.setState({ query });
+//     this.setState({ page: 1 });
+//   };
+//   onImageClick = (url, tags) => {
+//     this.setState({ largeImage: { url, tags } });
+//   };
+//   onHandleClose = () => {
+//     this.setState({ largeImage: { url: null, tags: null } });
+//   };
+//   onChangePage = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
+
+//   render() {
+//     return (
+//       <Container>
+//         <Searchbar onSubmit={this.onSubmit} />
+//         {this.state.isLoading && <Loader />}
+//         {this.state.images.length > 1 && (
+//           <ImageGallery
+//             images={this.state.images}
+//             onImageClick={this.onImageClick}
+//           />
+//         )}
+//         {this.state.images.length > 1 && (
+//           <Button onChangePage={this.onChangePage} />
+//         )}
+//         {this.state.largeImage.url && (
+//           <ModalWindow
+//             onHandleClose={this.onHandleClose}
+//             url={this.state.largeImage.url}
+//             tags={this.state.largeImage.tags}
+//           />
+//         )}
+//       </Container>
+//     );
+//   }
+// }
